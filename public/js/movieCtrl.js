ca9bbace3a1f4c7e4a26be57a2036203a7b6a7d7
Kurt <kurt@neumediasolutions.com>
2016-12-03 13:40:59 -0500

    fixed date picker for movie api call

--- public/js/movieCtrl.js
+++ public/js/movieCtrl.js
@@ -5,13 +5,11 @@ app.controller('movieCtrl', function($scope, $http) {
 
   $scope.findMovie = function(userZip, movieDate) {
 
-    var twenty = 20;
-    var movieDateVal = twenty + movieDate;
     var userZip = userZip;
     console.log(userZip);
     console.log(movieDate);
 
-    $http.get('http://data.tmsapi.com/v1.1/movies/showings?startDate=' + movieDateVal + '&zip=' + userZip + '&api_key=wud3ksj4patjm64jw9srnh8a').then (function successCallback(response) {
+    $http.get('http://data.tmsapi.com/v1.1/movies/showings?startDate=' + movieDate + '&zip=' + userZip + '&api_key=wud3ksj4patjm64jw9srnh8a').then (function successCallback(response) {
 
       var movieData = response;
       console.log(movieData);
