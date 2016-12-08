62a7643701a818839f1a3948e96702fda3836647
craigjleblanc <craigjleblanc@gmail.com>
2016-12-07 19:51:57 -0500

    Yelp API Food Displaying

--- public/js/foodCtrl.js
+++ public/js/foodCtrl.js
@@ -1,20 +1,34 @@
 var app = angular.module('dateNightMod');
 
-app.controller('foodCtrl', function($scope, $http, apiFactory) {
+app.controller('foodCtrl', function($scope, $http, $timeout, apiFactory) {
 
   var foodStuff;
 
-  apiFactory.getFoodInfo().then(function successCallback(response) {
-      foodStuff = response;
-  });
-  console.log(foodStuff);
-  $scope.foodName = foodStuff.food.data.businesses[0].name;
-  $scope.foodPhone = foodStuff.food.data.businesses[0].display_phone;
-  $scope.foodRating = foodStuff.food.data.businesses[0].rating;
-  $scope.foodClosed = foodStuff.food.data.businesses[0].is_closed;
+  $scope.sendFoodStuff = function(searchterm, searchlocation) {
+    console.log('sending food stuff done ' + searchterm + ' ' + searchlocation);
+    apiFactory.sendYelp(searchterm, searchlocation);
+
+    $timeout(function() {
+      foodStuff = apiFactory.retrieveYelp();
+    }, 1000);
+
+  };
+
+
+
+});
+
+  // apiFactory.getFoodInfo().then(function successCallback(response) {
+  //     foodStuff = response;
+  // });
+  //
+  // console.log(foodStuff);
+  // $scope.foodName = foodStuff.food.data.businesses[0].name;
+  // $scope.foodPhone = foodStuff.food.data.businesses[0].display_phone;
+  // $scope.foodRating = foodStuff.food.data.businesses[0].rating;
+  // $scope.foodClosed = foodStuff.food.data.businesses[0].is_closed;
   // yelpFactory.then(function(response) {
   //   $scope.restaurantName = response.data.businesses[0].name;
   //   $scope.restaurantReviewCount = response.data.businesses[0].review_count;
   //   console.log($scope.restaurant);
   // });
-});
--- public/js/inputFactory.js
+++ public/js/inputFactory.js
@@ -2,29 +2,60 @@ var app = angular.module('dateNightMod');
 
 app.factory('apiFactory', function($http) {
 
+
+ function randomString(length, chars) {
+      var result = '';
+      for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+      return result;
+  }
+
   var jsonObj = {};
-
-  var getFoodInfo = function() {
-    $http.get('/food').then(function successCallback(response) {
-      jsonObj.food = response;
-      console.log(jsonObj);
-    });
-  };
-
-  var getMovieInfo = function(jsonStuff) {
-    jsonObj.movie = jsonStuff;
-    console.log('Factory received movie info');
-  }
-
-  var sendJSON = function() {
-    console.log('Factory sending JSON to another controller');
-    return jsonObj;
-  }
+  var yelpObj = {};
 
   return {
-      getFoodInfo: getFoodInfo,
-      getMovieInfo: getMovieInfo,
-      sendJSON: sendJSON
-  }
+
+    getMovieInfo: function(jsonStuff) {
+      jsonObj.movie = jsonStuff;
+      console.log('Factory received movie info');
+    },
+
+    sendJSON: function() {
+      console.log('Factory sending JSON to another controller');
+      return jsonObj;
+    },
+
+    sendYelp: function(searchterm, searchlocation) {
+      yelpObj.term = searchterm;
+      yelpObj.location = searchlocation;
+      console.log(yelpObj);
+      return yelpObj;
+    },
+
+    retrieveYelp: function(name, callback) {
+         var method = 'GET';
+         var url = 'https://api.yelp.com/v2/search';
+         var params = {
+           callback: 'angular.callbacks._0',
+           location: yelpObj.location,
+           oauth_consumer_key: 'Gf_5Zit2VdKjs4trQHkFAA', //Consumer Key
+           oauth_token: '0hdeY89sEUBPuJcXsT6QsLqPpIC4dtnk', //Token
+           oauth_signature_method: "HMAC-SHA1",
+           oauth_timestamp: new Date().getTime(),
+           oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+           term: yelpObj.term
+         };
+         var consumerSecret = '4VIZvfeZrtZFFbHgzf97y2NsnIk'; //Consumer Secret
+         var tokenSecret = 'KCCjJtS_NyG1u8WKdryBKUmQ6ms'; //Token Secret
+         var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {
+           encodeSignature: false
+         });
+         params.oauth_signature = signature;
+         $http.jsonp(url, {
+           params: params
+         }).then(function successCallback(callback) {
+           console.log(callback);
+         });
+       }
+    };
 
 });
