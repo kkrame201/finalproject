54fe6f513c26d3b0ff3f84f4a0e61092c3c492b4
Kurt <kurt.e.kramer@gmail.com>
2016-12-09 07:39:46 -0500

    datepicker

--- public/js/inputCtrl.js
+++ public/js/inputCtrl.js
@@ -4,7 +4,10 @@ app.controller('inputCtrl', function($scope, $http, apiFactory, $location, $time
 
 $scope.findMovie = function(userSelection) {
   var userZip = userSelection.userZip;
-  $http.get('http://data.tmsapi.com/v1.1/movies/showings?startDate=' + userSelection.movieDate + '&zip=' + userSelection.userZip + '&api_key=wud3ksj4patjm64jw9srnh8a').then (function successCallback(response) {
+  var userDate = function(dateToString) {
+    toString(userSelection.movieDate);
+  }
+  $http.get('http://data.tmsapi.com/v1.1/movies/showings?startDate=' + userDate + '&zip=' + userSelection.userZip + '&api_key=wud3ksj4patjm64jw9srnh8a').then (function successCallback(response) {
     var movieData = response;
       console.log(movieData);
       apiFactory.getMovieInfo(movieData);
